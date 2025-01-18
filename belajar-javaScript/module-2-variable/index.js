cac0b3b00dbd6962d8cad47fe76a1ca52fcd5c30
Kiki-Ananda30 <m.riesqiananda@gmail.com>
2025-01-18 21:04:18 +0700

    Add files via upload

---
+++ belajar-javaScript/module-2-variable/index.js
@@ -0,0 +1,19 @@
+// var bisa diakses diluar scope/cakupan dan nilainya bisa diubah
+/*if (true) {
+  var name = "John Doe";
+}
+name = "Jane Doe";
+console.log("var: ", name);*/
+
+// let tidak bisa diakses diluar scope/cakupan dan nilainya bisa diubah seperti var
+/*if (true) {
+  let name = "John Doe";
+  name = "Jane Doe";
+  console.log("let: ", name);
+}*/
+
+// const tidak bisa diakses diluar scope/cakupan dan nilainya tidak bisa diubah
+/*if (true) {
+  const name = "John Doe";
+  console.log(" const: ", name);
+}*/
